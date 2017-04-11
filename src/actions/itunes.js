8cc5a400988eec9618e23f3f02c184b75be9cec3
bill-burgess <wmhburgess@gmail.com>
2017-04-11 16:29:59 +1200

    retreive iTunes api

--- src/actions/itunes.js
+++ src/actions/itunes.js
@@ -1,24 +1,25 @@
 import request from 'superagent'
 
 export const receiveArtist = (posts) => {
+  console.log("hello", posts)
   let result = {
     type: 'RECEIVE_ARTIST',
-    songs: posts.results.map(post => post.trackName)
+    songs: posts.map(post => post.trackName)
   }
-  console.log("hello", result);
   return result
 }
 
 export function fetchArtist (artist) {
   return (dispatch) => {
     request
-      .get(`https://itunes.apple.com/search?term=${artist}`)
+      .get(`http://localhost:3000`)
       .end((err, res) => {
         if (err) {
           console.error(err.message)
           return
         }
-        dispatch(receiveArtist(res.body))
+        console.log(res);
+        dispatch(receiveArtist(JSON.parse(res.text).results))
       })
   }
 }
